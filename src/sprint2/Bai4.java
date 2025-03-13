c2c8e5fec9bf1145a262aa3810bd7e4fd02a8c58
Letung1611 <155080062+Letung1611@users.noreply.github.com>
2025-03-13 08:43:13 +0700

    Add files via upload

--- src/sprint2/Bai4.java
+++ src/sprint2/Bai4.java
@@ -4,10 +4,52 @@
  */
 package sprint2;
 
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
 /**
  *
- * @author ACER
+ * @author M.S.I
  */
 public class Bai4 {
-    
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Tìm số nhỏ nhất");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+        frame.setLayout(new GridLayout(4, 1));
+        
+        JTextField txtA = new JTextField();
+        JTextField txtB = new JTextField();
+        JButton btnFindMin = new JButton("Tìm số nhỏ nhất");
+        JLabel lblResult = new JLabel("Kết quả: ", SwingConstants.CENTER);
+        
+        btnFindMin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    double a = Double.parseDouble(txtA.getText());
+                    double b = Double.parseDouble(txtB.getText());
+                    double min = Math.min(a, b);
+                    lblResult.setText("Số nhỏ nhất: " + min);
+                } catch (NumberFormatException ex) {
+                    lblResult.setText("Vui lòng nhập số hợp lệ!");
+                }
+            }
+        });
+        
+        frame.add(txtA);
+        frame.add(txtB);
+        frame.add(btnFindMin);
+        frame.add(lblResult);
+        
+        frame.setVisible(true);
+    }
 }
+
+
